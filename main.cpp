a902d2403775de4ea351bdb3815db78151ba984c
vinciible <148741992+vinciible@users.noreply.github.com>
2026-08-23 14:00:16 -0700

    Added third message on GitHub main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello GitHub!\n";
     std::cout << "this is a second message \n";
+    std::cout << "this message was added from GitHub to test pulling into local Git. \n";
     return 0;
 }
